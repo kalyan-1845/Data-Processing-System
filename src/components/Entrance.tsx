a9ed557f7adbac7af63510cf5937fc0a558085c6
kalyan-1845 <prsnlkalyan2gmail.com>
2026-04-01 18:25:53 +0530

    Syncing theme accents v118

--- src/components/Entrance.tsx
+++ src/components/Entrance.tsx
@@ -440,3 +440,5 @@ export function Entrance({ onComplete, onPhaseChange, onHoverChange }: {
 // version control incremental update 116
 
 // version control incremental update 117
+
+// version control incremental update 118
