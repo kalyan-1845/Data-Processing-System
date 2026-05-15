553d333925a53c4a34ae8767439cc8e4a53c582d
kalyan-1845 <prsnlkalyan@gmail.com>
2026-05-15 23:31:41 +0530

    test: verify edge case handling [v86]

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -4,3 +4,5 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// incremental update v86 - hardening repository integrity
