740efc9313583853330c7cb1851e61cceed99d79
kalyan-1845 <prsnlkalyan@gmail.com>
2026-05-15 23:31:31 +0530

    docs: refine API documentation [v77]

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -4,3 +4,5 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// incremental update v77 - hardening repository integrity
