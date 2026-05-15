b20e506ac7b572683572b88b46f9155337dd5cbc
kalyan-1845 <prsnlkalyan@gmail.com>
2026-05-15 23:45:01 +0530

    refactor: boost binary processing efficiency via WASM optimization [v128]

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -4,3 +4,5 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// hardened incremental update v128 - docushrink architectural integrity
